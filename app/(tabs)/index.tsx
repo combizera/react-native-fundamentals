48a43505a5f6cc0b8af7999787415c951b41f6ee
combizera <ycombi349@gmail.com>
2025-03-03 15:36:42 -0300

    feat: stylesheet in react-native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,34 @@
 import { Fragment } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-      <Text>Hello React!</Text>
-    </ View>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Hello World!
+      </Text>
+      <Text style={styles.eventDescription}>
+        Hello React!
+      </Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c1c1c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  eventDescription: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  }
+});
